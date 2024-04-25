38664990c1ad5e93f12f431db3c0c7db4b3fa009
Fellipe Juncal <fjuncal7@gmail.com>
2024-04-25 17:21:48 -0300

    Creating new endpoint to return a ChatResponse Class

--- src/main/java/com/springai/bookstore/ai/rest/controllers/BookstoreAssistantController.java
+++ src/main/java/com/springai/bookstore/ai/rest/controllers/BookstoreAssistantController.java
@@ -1,5 +1,7 @@
 package com.springai.bookstore.ai.rest.controllers;
 
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/bookstore")
 public class BookstoreAssistantController {
-    private final OpenAiChatClient openAiChatClient;
+    private final OpenAiChatClient ChatClient;
 
-    public BookstoreAssistantController(OpenAiChatClient openAiChatClient) {
-        this.openAiChatClient = openAiChatClient;
+    public BookstoreAssistantController(OpenAiChatClient ChatClient) {
+        this.ChatClient = ChatClient;
     }
 
     @GetMapping("/informations")
     public String bookStoreChat(@RequestParam(value = "message", defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message){
-        return openAiChatClient.call(message);
+        return ChatClient.call(message);
+    }
+
+    @GetMapping("/informationsChatResponse")
+    public ChatResponse bookStoreChatParameter(@RequestParam(value = "message", defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message){
+        return ChatClient.call(new Prompt(message));
     }
 }
